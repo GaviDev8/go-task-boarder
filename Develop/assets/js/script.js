959b7f2cbe53a350e8ec91b796af73e0bc6af603
Gabriela Ortiz <gabrielaortiz@Gabrielas-Air.lan>
2024-04-04 22:23:59 -0700

    HTML Code cleaned

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -3,36 +3,66 @@ let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
-function generateTaskId() {
+const generateTaskId = () => {
 
 }
 
 // Todo: create a function to create a task card
-function createTaskCard(task) {
+const createTaskCard = (task) => {
 
 }
 
 // Todo: create a function to render the task list and make cards draggable
-function renderTaskList() {
+const renderTaskList = () => {
+    if(!taskList) {
+        taskList = [];
+    }
+    const toDoList = $("#todo-cards");
+    toDoList.empty();
+
+    const inProgressList = $("#in-progress-cards");
+    inProgressList.empty();
+
+    const doneList = $("#done-cards");
+    doneList.empty()
+    
+    for(let i= 0; i < taskList.length; i++) {
+        if(taskList[i].status === "to-do") {
+            toDoList.append(createTaskCard(taskList[index]));
+        }
+        // else if ip and else if for done
+
+    }
+
+    $(".draggable").draggable({
+        opacity: 0.7,
+        zIndex: 100
+        
+        helper: function(event) {
+            const original = $(event.target);
+        }
+    }
+
+    )
 
 }
 
 // Todo: create a function to handle adding a new task
-function handleAddTask(event){
+const handleAddTask= (event) => {
 
 }
 
 // Todo: create a function to handle deleting a task
-function handleDeleteTask(event){
+const handleDeleteTask = (event) => {
 
 }
 
 // Todo: create a function to handle dropping a task into a new status lane
-function handleDrop(event, ui) {
+const handleDrop = (event, ui) => {
 
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
-$(document).ready(function () {
+$(document).ready(() => {
 
 });
